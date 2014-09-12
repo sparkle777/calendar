acc6ed436dcee926fcf2d559091443d998bf07c2
Shuqi Chan <shuqi0927@vip.qq.com>
2014-09-12 14:36:01 +0800

    bug fix && 增加年份和月份的控制

--- calendar.js
+++ calendar.js
@@ -1,34 +1,35 @@
 var currentYear;
 var currentMonth;
 var calendarList;
+var selectedYear;
+var selectedMonth;
+
 
 window.onload = function() {
 	var now = new Date();
 
 	currentYear = now.getFullYear();
-	currentMonth = now.getMonth();
+	currentMonth = now.getMonth(); //0表示1月，11表示12月
 	initialCalendar();
 	showTime();
 }
 
 function initialCalendar() {
-	var calendarLength;
 	var headLength;
 	var left_pos;
 	var top_pos;
-	var calendarItem;
 	var headList;
 	var headItem;
+	var startYear;
+	var endYear;
 
 	left_pos = 0;
 	top_pos = 0;
 
-	calendarList = document.getElementsByTagName("li");
-	calendarLength = calendarList.length;
-	headList = document.getElementById("head").getElementsByTagName("div");
+	headList = document.getElementsByClassName("headItem");
 	headLength = headList.length;
 
-	// 初始化日历的排列
+	// 初始化日历的头部
 	for (var count = 0; count < headLength; count++) {
 		headItem = headList[count];
 		headItem.style.left = left_pos + "px";
@@ -36,6 +37,36 @@ function initialCalendar() {
 
 		left_pos += 150;
 	}
+
+	// 初始化年份下拉框
+	startYear = currentYear - 100;
+	endYear = currentYear + 100;
+	selectedYear = document.getElementById("years");
+	for (var year = startYear; year < endYear; year++) {
+		selectedYear.options.add(new Option(year, year))
+	}
+	selectedYear.options[selectedYear.options.length-100].selected = "selected";
+
+	//初始化月份下拉框
+	selectedMonth = document.getElementById("months");
+	for (var month = 1; month < 13; month++) {
+		selectedMonth.options.add(new Option(month, month));
+		if (month == currentMonth+1)
+			selectedMonth.options[month-1].selected = "selected";
+	}
+
+	initialCalendarItem();
+}
+
+// 初始化日历项
+function initialCalendarItem() {
+	var calendarLength;
+	var calendarItem;
+	var left_pos;
+	var top_pos;
+
+	calendarList = document.getElementsByTagName("li");
+	calendarLength = calendarList.length;
 
 	left_pos = 0;
 	top_pos = -50;
@@ -54,25 +85,26 @@ function initialCalendar() {
 
 		left_pos += 100;
 	}
-	// 显示日历项的日期
-	initialCalendarItem();
+
+	showCalendarItem();
 }
 
-function initialCalendarItem() {
-	var firstDayOfCurrentMonth;
+// 显示日历项
+function showCalendarItem() {
+	var firstDateOfCurrentMonth;
 	var startWeekDay; // what day is it the first day of current month 
 	var startDate; // the date should be show at the first item
 	var daysOfCurrentMonth;
 
 	length = calendarList.length;
-	firstDayOfCurrentMonth = new Date(currentYear, currentMonth, 1);
-	startWeekDay = firstDayOfCurrentMonth.getDay();
-	if (startWeekDay != 7) {
-		daysOfCurrentMonth = (new Date(currentYear, currentMonth-1, 0)).getDate();
+	firstDateOfCurrentMonth = new Date(currentYear, currentMonth, 1);
+	startWeekDay = firstDateOfCurrentMonth.getDay();
+	if (startWeekDay != 0) {
+		daysOfCurrentMonth = (new Date(currentYear, currentMonth, 0)).getDate();
 		startDate = daysOfCurrentMonth - startWeekDay + 1;
 	}
 	else {
-		daysOfCurrentMonth = (new Date(currentYear, currentMonth, 0)).getDate();
+		daysOfCurrentMonth = (new Date(currentYear, currentMonth+1, 0)).getDate();
 		startDate = 1;
 	}
 
@@ -82,7 +114,7 @@ function initialCalendarItem() {
 		startDate++;
 		if (startDate > daysOfCurrentMonth) {
 			startDate = 1;
-			daysOfCurrentMonth = (new Date(currentYear, currentMonth, 0)).getDate();
+			daysOfCurrentMonth = (new Date(currentYear, currentMonth+1, 0)).getDate();
 		} 
 	}	
 }
@@ -100,8 +132,20 @@ function showTime() {
 	}, 1000);
 }
 
+function changeYear() {
+	var selectedIndex;
+	selectedIndex = selectedYear.selectedIndex;
+	currentYear = selectedYear.options[selectedIndex].value; 
+	showCalendarItem();
+}
 
-
+function changeMonth() {
+	var selectedIndex;
+	selectedIndex = selectedMonth.selectedIndex;
+	currentMonth = selectedMonth.options[selectedIndex].value; 
+	currentMonth -= 1;
+	showCalendarItem();
+}
 
 
 
